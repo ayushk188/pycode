f3b563006234089bd33dcf817dfa09c954903880
ayushk188 <152911522+ayushk188@users.noreply.github.com>
2023-12-05 22:11:58 +0530

    added python code

---
+++ 2nd.py
@@ -0,0 +1,16 @@
+# a="ayush"
+# b="kamboj"
+# #s=a+" "+b
+# print(a+" "+b)
+# a=12
+# print("my age is",a)
+# name=input("What is your name: ")
+# age=int(input("How old are you: "))
+# age+=1
+# print("my name is:",name)
+# print(age)
+# print("i am "+str(age)+" year old")
+a="My name is ayush kamboj "
+print(a[11:])
+b=slice(11,-1)
+print(a[b])
